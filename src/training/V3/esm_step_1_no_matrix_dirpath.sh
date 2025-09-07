94d7d022828711cfa12617bbaeacdf38e6ab8cc0
sbc806 <76720208+sbc806@users.noreply.github.com>
2025-09-07 16:48:46 -0700

    Update esm_step_1_no_matrix_dirpath.sh

--- src/training/V3/esm_step_1_no_matrix_dirpath.sh
+++ src/training/V3/esm_step_1_no_matrix_dirpath.sh
@@ -41,7 +41,7 @@ num_hidden_layers=4
 SEQ_POOLING_TYPE="value_attention"
 # word-level
 codes_file="step_1_all_sequences_corpus_codes_30000.txt"
-seq_subword="step_1_all_sequences_corpus_subword_vocab_20000.txt"
+seq_subword="step_1_all_sequences_corpus_subword_vocab_30000.txt"
 
 ## for embedding channel
 embedding_input_size=2560
