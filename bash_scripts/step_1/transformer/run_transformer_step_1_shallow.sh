d4eccd4244d74f34593a8f24ae1ba57ebac207de
sbc806 <sarahc19233@gmail.com>
2025-09-10 04:45:36 -0400

    Added bash scripts.

---
+++ bash_scripts/step_1/transformer/run_transformer_step_1_shallow.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH --account=def-guanuofa
+#SBATCH --gpus-per-node=a100:1
+#SBATCH --mem=64G
+#SBATCH --time=7-0
+#SBATCH --job-name=transformer-step-1
+#SBATCH --output=transformer_step_1_%j.out
+#SBATCH --err=transformer_step_1_%j.err
+
+
+module load python/3.11
+module load scipy-stack
+module load gcc arrow/19.0.1
+
+
+cd /home/schen123/projects/def-guanuofa/schen123/kinases/virtual_environments
+source TEST/bin/activate
+
+
+cd ../sbc806/LucaPCycle/src/training/V3
+cat transformer_step_1.sh > /home/schen123/projects/def-guanuofa/schen123/kinases/bash_scripts/step_1/transformer/transformer_step_1_$SLURM_JOB_ID.txt
+./transformer_step_1.sh
+
+
+deactivate
