b568f187cb3ad709af81fdd812de367a0e49b876
sbc806 <76720208+sbc806@users.noreply.github.com>
2025-09-13 22:14:47 -0700

    Update run_esm_step_2_no_matrix_dirpath.sh

--- bash_scripts/step_2/esm/run_esm_step_2_no_matrix_dirpath.sh
+++ bash_scripts/step_2/esm/run_esm_step_2_no_matrix_dirpath.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH --account=def-guanuofa
-#SBATCH --gpus=nvidia_h100_80gb_hbm3_3g.40gb:1
-#SBATCH --mem=128G
+#SBATCH --gpus-per-node=h100:1
+#SBATCH --mem=200G
 #SBATCH --time=7-0
 #SBATCH --job-name=esm-step-2-no-matrix-dirpath
-#SBATCH --output=esm_step_2_no_matrix_dirpath_%j.out
-#SBATCH --err=esm_step_2_no_matrix_dirpath_%j.err
+#SBATCH --output=output/esm_step_2_no_matrix_dirpath_%j.out
+#SBATCH --err=output/esm_step_2_no_matrix_dirpath_%j.err
 
 
 module load python/3.11
@@ -18,7 +18,7 @@ source TEST/bin/activate
 
 
 cd ../sbc806/RumHKNet/src/training/V3
-cat esm_step_2_no_matrix_dirpath.sh > /home/schen123/scratch/kinases/bash_scripts/step_2/esm/esm_step_2_no_matrix_dirpath_$SLURM_JOB_ID.txt
+cat esm_step_2_no_matrix_dirpath.sh > /home/schen123/scratch/kinases/bash_scripts/step_2/esm/output/esm_step_2_no_matrix_dirpath_$SLURM_JOB_ID.txt
 ./esm_step_2_no_matrix_dirpath.sh
 
 
