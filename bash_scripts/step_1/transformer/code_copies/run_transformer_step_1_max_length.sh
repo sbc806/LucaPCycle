997e3ac90244e289f6e4da47be2d8143f6a93439
sbc806 <sarahc19233@gmail.com>
2025-09-15 19:03:19 -0400

    Adding scripts.

---
+++ bash_scripts/step_1/transformer/code_copies/run_transformer_step_1_max_length.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH --account=def-guanuofa
+#SBATCH --gpus-per-node=a100:1
+#SBATCH --mem=32G
+#SBATCH --time=7-0
+#SBATCH --job-name=transformer-step-1-max-length
+#SBATCH --output=transformer_step_1_max_length%j.out
+#SBATCH --err=transformer_step_1_max_length_%j.err
+
+
+module load python/3.11
+module load scipy-stack
+module load gcc arrow/19.0.1
+
+
+cd /home/schen123/projects/def-guanuofa/schen123/kinases/virtual_environments
+source TEST/bin/activate
+
+
+cd ../sbc806/LucaPCycle/src/training/V3
+cat transformer_step_1_max_length.sh > /home/schen123/projects/def-guanuofa/schen123/kinases/bash_scripts/step_1/transformer/transformer_step_1_max_length_$SLURM_JOB_ID.txt
+./transformer_step_1_max_length.sh
+
+
+deactivate
---
+++ bash_scripts/step_1/transformer/run_transformer_step_1_shallow_50.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH --account=def-guanuofa
+#SBATCH --gpus-per-node=a100:1
+#SBATCH --mem=32G
+#SBATCH --time=7-0
+#SBATCH --job-name=transformer-step-1-shallow
+#SBATCH --output=transformer_step_1_shallow_%j.out
+#SBATCH --err=transformer_step_1_shallow_%j.err
+
+
+module load python/3.11
+module load scipy-stack
+module load gcc arrow/19.0.1
+
+
+cd /home/schen123/projects/def-guanuofa/schen123/kinases/virtual_environments
+source TEST/bin/activate
+
+
+cd ../sbc806/LucaPCycle/src/training/V3
+cat transformer_step_1_shallow.sh > /home/schen123/projects/def-guanuofa/schen123/kinases/bash_scripts/step_1/transformer/transformer_step_1_shallow_$SLURM_JOB_ID.txt
+./transformer_step_1_shallow.sh
+
+
+deactivate
