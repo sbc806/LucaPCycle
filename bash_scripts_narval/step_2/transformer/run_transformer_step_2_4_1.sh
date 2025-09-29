352aff67cc734a5b3ed6651cf59cf09c1ea1309c
sbc806 <76720208+sbc806@users.noreply.github.com>
2025-09-29 16:51:47 -0700

    Update run_transformer_step_2_4_1.sh

--- bash_scripts_narval/step_2/transformer/run_transformer_step_2_4_1.sh
+++ bash_scripts_narval/step_2/transformer/run_transformer_step_2_4_1.sh
@@ -4,8 +4,8 @@
 #SBATCH --mem=32G
 #SBATCH --time=4-0
 #SBATCH --job-name=transformer-step-2-4-1
-#SBATCH --output=transformer_step_2_4_1_%j.out
-#SBATCH --err=transformer_step_2_4_1_%j.err
+#SBATCH --output=output/transformer_step_2_4_1_%j.out
+#SBATCH --err=output/transformer_step_2_4_1_%j.err
 
 
 module load python/3.11
@@ -17,8 +17,8 @@ cd /home/schen123/links/projects/def-guanuofa/schen123/kinases/virtual_environme
 source TEST/bin/activate
 
 
-cd ../sbc806/RumHKNet/src/training/V3
-cat ./transformer_step_2_4_1.sh > /home/schen123/links/projects/def-guanuofa/schen123/kinases/bash_scripts_narval/step_2/transformer/transformer_step_2_4_1_$SLURM_JOB_ID.txt
+cd ../sbc806_1/RumHKNet/src/training/V3
+cat ./transformer_step_2_4_1.sh > /home/schen123/links/projects/def-guanuofa/schen123/kinases/sbc806_1/RumHKNet/bash_scripts_narval/step_2/transformer/output/transformer_step_2_4_1_$SLURM_JOB_ID.txt
 ./transformer_step_2_4_1.sh
 
 
