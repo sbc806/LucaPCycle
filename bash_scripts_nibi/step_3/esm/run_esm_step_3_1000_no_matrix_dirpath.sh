e7a4bab778eb474fc5e167fa1213f486a1dfb89b
sbc806 <76720208+sbc806@users.noreply.github.com>
2025-09-29 12:53:20 -0700

    Update run_esm_step_3_1000_no_matrix_dirpath.sh

--- bash_scripts_nibi/step_3/esm/run_esm_step_3_1000_no_matrix_dirpath.sh
+++ bash_scripts_nibi/step_3/esm/run_esm_step_3_1000_no_matrix_dirpath.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --account=def-guanuofa
+#SBATCH --account=rrg-guanuofa
 #SBATCH --gpus=nvidia_h100_80gb_hbm3_3g.40gb:1
 #SBATCH --mem=128G
 #SBATCH --time=7-0
@@ -10,15 +10,15 @@
 
 module load python/3.11
 module load scipy-stack
-module load gcc arrow/19.0.1
+module load gcc arrow/21.0.0
 
 
-cd /home/schen123/scratch/kinases/virtual_environments
+cd /home/schen123/projects/rrg-guanuofa/schen123/kinases/virtual_environments
 source TEST/bin/activate
 
 
 cd ../sbc806/RumHKNet/src/training/V3
-cat esm_step_3_1000_no_matrix_dirpath.sh > /home/schen123/scratch/kinases/bash_scripts_fir/step_3/esm/output/esm_step_3_1000_no_matrix_dirpath_$SLURM_JOB_ID.txt
+cat esm_step_3_1000_no_matrix_dirpath.sh > /home/schen123/projects/rrg-guanuofa/schen123/kinases/sbc806/RumHKNet/bash_scripts_nibi/step_3/esm/output/esm_step_3_1000_no_matrix_dirpath_$SLURM_JOB_ID.txt
 ./esm_step_3_1000_no_matrix_dirpath.sh
 
 
