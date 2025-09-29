1eef6c2f4cb40c7ac31527f346b0387c39c84f23
sbc806 <76720208+sbc806@users.noreply.github.com>
2025-09-29 15:36:20 -0700

    Update transformer_step_1_4_1_intermediate_2048.sh

--- src/training/V3/transformer_step_1_4_1_intermediate_2048.sh
+++ src/training/V3/transformer_step_1_4_1_intermediate_2048.sh
@@ -32,11 +32,11 @@ loss_type="bce"
 
 
 ## for sequence channel
-SEQ_MAX_LENGTH=3072
+SEQ_MAX_LENGTH=3432
 hidden_size=1024
-intermediate_size=4096
-num_attention_heads=8
-num_hidden_layers=4
+intermediate_size=2048
+num_attention_heads=4
+num_hidden_layers=1
 ### pooling type: none, max, mean, value_attention
 SEQ_POOLING_TYPE="value_attention"
 # word-level
@@ -76,7 +76,7 @@ buffer_size=10240
 ## tokenizer dir
 tokenizer_dir="step_1"
 ## positive weight
-pos_weight=None
+pos_weight=0.4
 
 # model building time
 time_str=$(date "+%Y%m%d%H%M%S")
@@ -147,7 +147,7 @@ python run_seq_only.py \
   --classifier_activate_func gelu \
   --warmup_steps $warmup_steps \
   --beta1 0.9 \
-  --beta2 0.98 \
+  --beta2 0.99 \
   --weight_decay 0.01 \
   --save_steps $save_steps \
   --max_steps $max_steps \
