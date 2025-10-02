0dbbe2ee6040994549cad576346e52249dd8e4ed
sbc806 <76720208+sbc806@users.noreply.github.com>
2025-10-01 22:01:08 -0700

    Update run_esm_step_3.sh

--- bash_scripts_nibi/step_3/esm/run_esm_step_3.sh
+++ bash_scripts_nibi/step_3/esm/run_esm_step_3.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
-#SBATCH --account=def-guanuofa
-#SBATCH --gpus-per-node=a100:1
-#SBATCH --mem=256G
+#SBATCH --account=rrg-guanuofa
+#SBATCH --gpus-per-node=h100:1
+#SBATCH --mem=128G
 #SBATCH --time=7-0
 #SBATCH --job-name=esm-step-3
-#SBATCH --output=esm_step_3_%j.out
-#SBATCH --err=esm_step_3_%j.err
+#SBATCH --output=output/esm_step_3_%j.out
+#SBATCH --err=output/esm_step_3_%j.err
 
 
 module load python/3.11
 module load scipy-stack
-module load gcc arrow/19.0.1
+module load gcc arrow/21.0.0
 
 
-cd /home/schen123/projects/def-guanuofa/schen123/kinases/virtual_environments
+cd /home/schen123/projects/rrg-guanuofa/schen123/kinases/virtual_environments
 source TEST/bin/activate
 
 
-cd ../sbc806/LucaPCycle/src/training/V3
-cat esm_step_3.sh > /home/schen123/projects/def-guanuofa/schen123/kinases/bash_scripts/step_3/esm/esm_step_3_$SLURM_JOB_ID.txt
+cd ../sbc806/RumHKNet/src/training/V3
+cat esm_step_3.sh > /home/schen123/projects/rrg-guanuofa/schen123/kinases/sbc806/RumHKNet/bash_scripts_nibi/step_3/esm/output/esm_step_3_$SLURM_JOB_ID.txt
 ./esm_step_3.sh
 
 
