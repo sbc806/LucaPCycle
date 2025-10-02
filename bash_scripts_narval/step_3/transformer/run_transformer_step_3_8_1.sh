ed71b8b97e77f4a1767ac4d8c9a3400afd85f8eb
sbc806 <76720208+sbc806@users.noreply.github.com>
2025-10-02 11:30:40 -0700

    Update run_transformer_step_3_8_1.sh

--- bash_scripts_narval/step_3/transformer/run_transformer_step_3_8_1.sh
+++ bash_scripts_narval/step_3/transformer/run_transformer_step_3_8_1.sh
@@ -2,10 +2,10 @@
 #SBATCH --account=def-guanuofa
 #SBATCH --gpus-per-node=a100:1
 #SBATCH --mem=64G
-#SBATCH --time=7-0
-#SBATCH --job-name=transformer-step-3
-#SBATCH --output=transformer_step_3_%j.out
-#SBATCH --err=transformer_step_3_%j.err
+#SBATCH --time=4-0
+#SBATCH --job-name=transformer-step-3-8-1
+#SBATCH --output=output/transformer_step_3_8_1_%j.out
+#SBATCH --err=output/transformer_step_3_8_1_%j.err
 
 
 module load python/3.11
@@ -17,9 +17,9 @@ cd /home/schen123/projects/def-guanuofa/schen123/kinases/virtual_environments
 source TEST/bin/activate
 
 
-cd ../sbc806/LucaPCycle/src/training/V3
-cat transformer_step_3.sh > /home/schen123/projects/def-guanuofa/schen123/kinases/bash_scripts/step_3/transformer/transformer_step_3_$SLURM_JOB_ID.txt
-./transformer_step_3.sh
+cd ../sbc806/RumHKNet/src/training/V3
+cat transformer_step_3_8_1.sh > /home/schen123/projects/def-guanuofa/schen123/kinases/sbc806/RumHKNet/bash_scripts_narval/step_3/transformer/output/transformer_step_3_8_1_$SLURM_JOB_ID.txt
+./transformer_step_3_8_1.sh
 
 
 deactivate
