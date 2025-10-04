20a1290f10d7c3bf45da4294c1f47234297079fd
sbc806 <76720208+sbc806@users.noreply.github.com>
2025-10-03 21:06:58 -0700

    Update run_transformer_step_1_256_4096.sh

--- bash_scripts_nibi/step_1/transformer/run_transformer_step_1_256_4096.sh
+++ bash_scripts_nibi/step_1/transformer/run_transformer_step_1_256_4096.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#SBATCH --account=def-guanuofa
-#SBATCH --gpus-per-node=a100:1
+#SBATCH --account=rrg-guanuofa
+#SBATCH --gpus-per-node=h100:1
 #SBATCH --mem=32G
 #SBATCH --time=4-0
 #SBATCH --job-name=transformer-step-1-256-4096
@@ -13,12 +13,12 @@ module load scipy-stack
 module load gcc arrow/19.0.1
 
 
-cd /home/schen123/projects/def-guanuofa/schen123/kinases/virtual_environments
+cd /home/schen123/projects/rrg-guanuofa/schen123/kinases/virtual_environments
 source TEST/bin/activate
 
 
 cd ../sbc806/RumHKNet/src/training/V3
-cat transformer_step_1_256_4096.sh > /home/schen123/projects/def-guanuofa/schen123/kinases/sbc806/RumHKNet/bash_scripts_narval/step_1/transformer/output/transformer_step_1_256_4096_$SLURM_JOB_ID.txt
+cat transformer_step_1_256_4096.sh > /home/schen123/projects/rrg-guanuofa/schen123/kinases/sbc806/RumHKNet/bash_scripts_nibi/step_1/transformer/output/transformer_step_1_256_4096_$SLURM_JOB_ID.txt
 ./transformer_step_1_256_4096.sh
 
 
