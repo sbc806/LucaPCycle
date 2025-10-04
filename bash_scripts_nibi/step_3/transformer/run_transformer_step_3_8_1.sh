103121e853ac7185106e935fe50682d50f5b0bb7
sbc806 <76720208+sbc806@users.noreply.github.com>
2025-10-03 22:20:39 -0700

    Update run_transformer_step_3_8_1.sh

--- bash_scripts_nibi/step_3/transformer/run_transformer_step_3_8_1.sh
+++ bash_scripts_nibi/step_3/transformer/run_transformer_step_3_8_1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#SBATCH --account=def-guanuofa
-#SBATCH --gpus-per-node=a100:1
+#SBATCH --account=rrg-guanuofa
+#SBATCH --gpus-per-node=h100:1
 #SBATCH --mem=64G
 #SBATCH --time=4-0
 #SBATCH --job-name=transformer-step-3-8-1
@@ -10,15 +10,15 @@
 
 module load python/3.11
 module load scipy-stack
-module load gcc arrow/19.0.1
+module load gcc arrow/21.0.0
 
 
-cd /home/schen123/projects/def-guanuofa/schen123/kinases/virtual_environments
+cd /home/schen123/projects/rrg-guanuofa/schen123/kinases/virtual_environments
 source TEST/bin/activate
 
 
 cd ../sbc806/RumHKNet/src/training/V3
-cat transformer_step_3_8_1.sh > /home/schen123/projects/def-guanuofa/schen123/kinases/sbc806/RumHKNet/bash_scripts_narval/step_3/transformer/output/transformer_step_3_8_1_$SLURM_JOB_ID.txt
+cat transformer_step_3_8_1.sh > /home/schen123/projects/def-guanuofa/schen123/kinases/sbc806/RumHKNet/bash_scripts_nibi/step_3/transformer/output/transformer_step_3_8_1_$SLURM_JOB_ID.txt
 ./transformer_step_3_8_1.sh
 
 
