62dd703dfb47065a10981622849fce12c9dcdf07
sbc806 <76720208+sbc806@users.noreply.github.com>
2025-10-03 22:34:07 -0700

    Update run_transformer_step_3_inverse_divide5.sh

--- bash_scripts_nibi/step_3/transformer/run_transformer_step_3_inverse_divide5.sh
+++ bash_scripts_nibi/step_3/transformer/run_transformer_step_3_inverse_divide5.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-#SBATCH --account=def-guanuofa
-#SBATCH --gpus-per-node=a100:1
+#SBATCH --account=rrg-guanuofa
+#SBATCH --gpus-per-node=h100:1
 #SBATCH --mem=64G
-#SBATCH --time=7-0
-#SBATCH --job-name=transformer-step-3
-#SBATCH --output=transformer_step_3_%j.out
-#SBATCH --err=transformer_step_3_%j.err
+#SBATCH --time=4-0
+#SBATCH --job-name=transformer-step-3-inverse-divide5
+#SBATCH --output=output/transformer_step_3_inverse_divide5_%j.out
+#SBATCH --err=output/transformer_step_3_inverse_divide5_%j.err
 
 
 module load python/3.11
 module load scipy-stack
-module load gcc arrow/19.0.1
+module load gcc arrow/21.0.0
 
 
 cd /home/schen123/projects/def-guanuofa/schen123/kinases/virtual_environments
