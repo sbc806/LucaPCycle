71b66f205e52afd60912604d78d9d8237c75cd61
sbc806 <76720208+sbc806@users.noreply.github.com>
2025-10-11 02:26:01 -0700

    Update run_transformer_step_1_b8_g2.sh

--- bash_scripts_nibi/step_1/transformer/run_transformer_step_1_b8_g2.sh
+++ bash_scripts_nibi/step_1/transformer/run_transformer_step_1_b8_g2.sh
@@ -3,14 +3,14 @@
 #SBATCH --gpus-per-node=h100:1
 #SBATCH --mem=32G
 #SBATCH --time=4-0
-#SBATCH --job-name=transformer-step-1-4-1-512-2048
-#SBATCH --output=output/transformer_step_1_4_1_512_2048_%j.out
-#SBATCH --err=output/transformer_step_1_4_1_512_2048_%j.err
+#SBATCH --job-name=transformer-step-1-b8-g2
+#SBATCH --output=output/transformer_step_1_b8_g2_%j.out
+#SBATCH --err=output/transformer_step_1_b8_g2_%j.err
 
 
 module load python/3.11
 module load scipy-stack
-module load gcc arrow/19.0.1
+module load gcc arrow/21.0.0
 
 
 cd /home/schen123/projects/rrg-guanuofa/schen123/kinases/virtual_environments
